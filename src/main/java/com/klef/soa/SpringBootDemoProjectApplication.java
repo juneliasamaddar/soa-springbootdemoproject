2e515a437839bd8bf0f7ecb6b64fb5e377f35884
juneliasamaddar <samaddarjunelia@gmail.com>
2026-07-28 17:02:09 +0530

    implemented restTemplate

--- src/main/java/com/klef/soa/SpringBootDemoProjectApplication.java
+++ src/main/java/com/klef/soa/SpringBootDemoProjectApplication.java
@@ -2,6 +2,8 @@ package com.klef.soa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootDemoProjectApplication {
@@ -9,6 +11,11 @@ public class SpringBootDemoProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoProjectApplication.class, args);
 		System.out.println("Project is running...!!!");
+	}
+	@Bean
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
 	}
 
 }
---
+++ src/main/java/com/klef/soa/dto/ProductDTO.java
@@ -0,0 +1,40 @@
+package com.klef.soa.dto;
+
+public class ProductDTO 
+{
+	private int id;
+	private String title;
+	private double price;
+	private String description;
+	private String category;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}
